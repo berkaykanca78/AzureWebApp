71ff25c3c66ee05311829b2ca5d51458c0462591
Berkay Kanca <berkaykanca@hotmail.com>
2024-10-01 00:56:19 +0300

    Bug Fix

--- Program.cs
+++ Program.cs
@@ -15,7 +15,7 @@ namespace AzureWebApp.API
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            builder.Services.AddSwaggerGen(); 
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("ProdConnection")));
 
