fdb976f8003b8ca8757da4fca411c4731bcc8631
Berkay Kanca <berkaykanca@hotmail.com>
2024-10-01 02:46:24 +0300

    Değişiklikler yapıldı.

--- Program.cs
+++ Program.cs
@@ -10,10 +10,7 @@ namespace AzureWebApp.API
         {
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-
             builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
@@ -26,8 +23,14 @@ namespace AzureWebApp.API
             app.MapGet("/", () => "Welcome My API Home Page!");
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            using (var scope = app.Services.CreateScope())
+            {
+                var srv = scope.ServiceProvider;
+                var context = srv.GetRequiredService<ApplicationDbContext>();
+                context.Database.Migrate();
+            }
 
             app.Run();
         }
