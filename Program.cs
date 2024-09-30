4beaec77e1cf57815eeb04eb7c4825959066c0a6
Berkay Kanca <berkaykanca@hotmail.com>
2024-10-01 01:14:13 +0300

    bf

--- Program.cs
+++ Program.cs
@@ -16,8 +16,8 @@ namespace AzureWebApp.API
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen(); 
-            //builder.Services.AddDbContext<ApplicationDbContext>(options =>
-            //    options.UseSqlServer(builder.Configuration.GetConnectionString("ProdConnection")));
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(builder.Configuration.GetConnectionString("ProdConnection")));
 
             var app = builder.Build();
 
